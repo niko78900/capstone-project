9c2e2a2fd1deccee1e09a48d13e9748af73ae99e
Niko <nikolasiko932@gmail.com>
2026-05-17 15:12:55 +0200

    point mobile app to sectora server

--- mobileapp/cap_app/lib/core/config/app_config.dart
+++ mobileapp/cap_app/lib/core/config/app_config.dart
@@ -3,6 +3,6 @@ class AppConfig {
 
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://10.0.2.2:8080',
+    defaultValue: 'http://sectora.ddns.net',
   );
 }
